80b82530c72313bf5e60aa81bdf63d07335acc4c
Annrie Rosales <annr1erosaless@gmail.com>
2024-08-28 12:08:13 +0800

    Create string-handling.py

---
+++ Exercise Set 02/string-handling.py
@@ -0,0 +1,5 @@
+color_1 = input("Enter first color: ")
+color_2 = input("Enter second color: ")
+color_3 = input("Enter third color: ")
+
+print(f"{color_1}, {color_2}, {color_3} created new color!")
