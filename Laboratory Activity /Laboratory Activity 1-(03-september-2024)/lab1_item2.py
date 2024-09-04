f2141beb73daec3810dea99a3e320f68d607dd5d
Annrie Rosales <annr1erosaless@gmail.com>
2024-09-04 16:16:32 +1200

    item 2

---
+++ Laboratory Activity /Laboratory Activity 1-(03-september-2024)/lab1_item2.py
@@ -0,0 +1,4 @@
+char1, char2 = input("Enter two space-separated characters: ").split()
+greater_char = max (char1, char2)
+print("---------------------")
+print(f"The character with greater value is {greater_char}.")
