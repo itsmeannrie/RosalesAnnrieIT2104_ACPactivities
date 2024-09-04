ee5ab75a3565808c864c4096c16b6eadee37ddd8
Annrie Rosales <annr1erosaless@gmail.com>
2024-09-04 15:15:00 +1200

    001/ september-04-2024

--- Laboratory Activity /Laboratory Activity 1-(03-september-2024)/lab_item1.py
+++ Laboratory Activity /Laboratory Activity 1-(03-september-2024)/lab_item1.py
@@ -1,1 +1,20 @@
+def main():
+    year = int(input("Enter the year: "))
+    album = input("Enter the album: ")
+    genre = input("Enter the genre: ")
+    title = input("Enter the song title: ")
+    artist = input("Enter the artist: ")
+
+    print("-------------")
+    print("SONG DETAILS")
+    print("-------------")
+
+    print(f"\nYear Released: {year}")
+    print(f"Genre: {genre}")
+    print(f"Album: {album}")
+    print(f'Title: "{title}"')
+    print(f"Artist: {artist}")
+
+if __name__ == "__main__":
+    main()
 
